0a12403afa4067859d9e13ab99f2b3a3fd70f380
ruan-silva1 <ruan.jdsilva@senacsp.edu.br>
2025-03-11 10:45:07 -0300

    adicionando funcoes ao user

--- projeto-6-kotlin/src/main/kotlin/Main.kt
+++ projeto-6-kotlin/src/main/kotlin/Main.kt
@@ -12,10 +12,12 @@ class User{
     val nomes = mutableListOf<String>()
     val enderecos = mutableListOf<String>()
     val telefones = mutableListOf<String>()
+    val medias = mutableListOf<Double>()
 
    init{
-        var i = 0
-        while(i<2){
+        repeat(2){
+            var i = 1
+            var soma = 0.0
             println("digite o nome do usuário")
             val nome = readln()
             nomes.add(nome)
@@ -25,7 +27,22 @@ class User{
             println("digite o telefone do usuario")
             val telefone = readln()
             telefones.add(telefone)
-            i++
+            println("Qual a quantidade de notas atribuitas?")
+            val qtdeNotas = readln().toInt()
+            while(i<=qtdeNotas){
+                println("digite a $i nota")
+                var notas = readln().toDouble()
+                while(notas < 0.0 || notas > 10.0){
+                    println("nota invalida!")
+                    println("digite a $i nota")
+                     notas = readln().toDouble()
+                }
+                soma+=notas
+                i++
+            }
+            val media = soma / qtdeNotas
+            medias.add(media)
+
         }
     }
 
@@ -38,7 +55,9 @@ class User{
                     usuarioEncontrado = true
                     val enderecoUser = enderecos[i]
                     val telefoneUser = telefones[i]
-                    println("O usuário $nome mora em $enderecoUser e tem o seguinte telefone $telefoneUser")
+                    val media = medias[i]
+                    val aprovacao = if(media > 5) "aprovado" else "desaprovado"
+                    println("O usuário $nome mora em $enderecoUser e tem o seguinte telefone $telefoneUser sua media foi $media e ele foi $aprovacao")
                 }
             }
         if(usuarioEncontrado == false){
@@ -48,11 +67,13 @@ class User{
 
     fun mostrarRegistros(){
         var i=0
+        val aprovacao = if(medias[i] > 5) "aprovado" else "desaprovado"
+        println("usuario | telefone | endereço | media | aprovacao")
         while(i<nomes.size){
-            println("usuario | telefone | endereço")
-            println("${nomes[i]}| ${telefones[i]} | ${enderecos[i]}")
+            println("${nomes[i]}| ${telefones[i]} | ${enderecos[i]} | ${medias[i]} | ${aprovacao}")
             i++
         }
     }
 
 }
+
